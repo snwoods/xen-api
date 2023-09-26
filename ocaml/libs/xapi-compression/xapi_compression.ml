8690793f45d8de2eb97365230d406c4ed98bd8c0
Steven Woods <steven.woods@citrix.com>
2023-09-26 13:34:43 +0100

    Adding zstd to the dune file

--- ocaml/libs/xapi-compression/xapi_compression.ml
+++ ocaml/libs/xapi-compression/xapi_compression.ml
@@ -146,6 +146,6 @@ module Make (Algorithm : ALGORITHM) = struct
       )
 
   let compress_file compress_fn file_path =
-    Xapi_stdext_unix.Unixext.with_file (file_path ^ ".zstd") [O_WRONLY] 0o444
+    ignore @@ Xapi_stdext_unix.Unixext.with_file (file_path ^ ".zstd") [O_WRONLY] 0o444
       compress_fn (write_to ~file_path)
 end
