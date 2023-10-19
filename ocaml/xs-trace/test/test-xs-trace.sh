0c297259928c9e47d39d89d4896c09b6205a1cfb
Steven Woods <steven.woods@citrix.com>
2023-10-19 16:17:36 +0100

    Fix PR comments

--- ocaml/xs-trace/test/test-xs-trace.sh
+++ ocaml/xs-trace/test/test-xs-trace.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -eux
 
 export PORT=9411
@@ -9,27 +10,18 @@ PID=$!
 
 ../xs_trace.exe cp test-source.json http://$HOST:$PORT/api/v2/spans
 
-if [ $(diff test-source.json test-http-server.out) -ne 0 ]
-then
-    exit 1
-fi
+diff test-source.json test-http-server.out || exit 1
 
 rm test-http-server.out
 
 ../xs_trace.exe cp test-source.ndjson http://$HOST:$PORT/api/v2/spans
 
-if [ $(diff test-source.ndjson test-http-server.out) -ne 0 ]
-then
-    exit 1
-fi
+diff test-source.ndjson test-http-server.out || exit 1
 
 rm test-http-server.out
 
 ../xs_trace.exe cp test-source.ndjson.zst http://$HOST:$PORT/api/v2/spans
 
-if [ $(diff test-source.ndjson test-http-server.out) -ne 0 ]
-then
-    exit 1
-fi
+diff test-source.ndjson test-http-server.out || exit 1
 
 kill $PID
--- ocaml/xs-trace/test/test_xs_trace.ml
+++ ocaml/xs-trace/test/test_xs_trace.ml
@@ -1,19 +1,19 @@
 let _ =
   let inet_addr = Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 9411) in
   let read_body ic file len =
-    let line = really_input_string ic !len in
+    let line = really_input_string ic len in
     Printf.fprintf file "%s\n" line
   in
-  let len = ref 0 in
-  let rec read_header ic file =
+  let rec read_header ic file len =
     match input_line ic with
-    | line when line = "\r" ->
+    | "\r" ->
         read_body ic file len
     | line ->
         if String.starts_with ~prefix:"Content-Length: " line then
           (*Subtract one more than the length of Content-Length: so we don't read the trailing \r*)
-          len := int_of_string (String.sub line 16 (String.length line - 17)) ;
-        read_header ic file
+          read_header ic file (int_of_string (String.sub line 16 (String.length line - 17)))
+        else
+          read_header ic file len
     | exception End_of_file ->
         ()
   in
@@ -24,6 +24,6 @@ let _ =
           [Open_wronly; Open_append; Open_creat]
           0o600 "test-http-server.out"
       in
-      read_header ic file ; close_out file
+      read_header ic file 0 ; close_out file
     )
     inet_addr
