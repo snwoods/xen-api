603e0cce5c769a674df47e6f30d2e9820373f172
Steven Woods <steven.woods@citrix.com>
2023-10-19 11:52:45 +0100

    Remove compressed file test

--- ocaml/xs-trace/test/test-xs-trace.sh
+++ ocaml/xs-trace/test/test-xs-trace.sh
@@ -22,13 +22,4 @@ then
     exit 1
 fi
 
-rm test-http-server.out
-
-../xs_trace.exe cp test-source.ndjson.zst file://$PATH_TO_FILE/test-socket
-
-if [ $(diff test-source.ndjson test-http-server.out) -ne 0 ]
-then
-    exit 1
-fi
-
 kill $PID
