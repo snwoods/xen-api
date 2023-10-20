0f4c0e288e3f0d84205210a7c1d5a110424fca6b
Steven Woods <steven.woods@citrix.com>
2023-10-20 16:56:01 +0100

    Ignore differences in whitespace

--- ocaml/xs-trace/test/test-xs-trace.sh
+++ ocaml/xs-trace/test/test-xs-trace.sh
@@ -17,12 +17,12 @@ echo "Testing"
 
 ../xs_trace.exe cp test-source.json file://$PATH_TO_FILE/test-socket
 
-diff test-source.json test-http-server.out || exit 1
+diff -B test-source.json test-http-server.out || exit 1
 
 rm test-http-server.out
 
 ../xs_trace.exe cp test-source.ndjson file://$PATH_TO_FILE/test-socket
 
-diff test-source.ndjson test-http-server.out || exit 1
+diff -B test-source.ndjson test-http-server.out || exit 1
 
 kill $PID
