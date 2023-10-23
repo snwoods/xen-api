7f766c8ed5d981d5cdb1426525ef6850e439854a
Steven Woods <steven.woods@citrix.com>
2023-10-23 17:15:13 +0100

    Add CSG copyright and fix whitespace in test

--- ocaml/xs-trace/test/test_xs_trace.ml
+++ ocaml/xs-trace/test/test_xs_trace.ml
@@ -1,3 +1,17 @@
+(*
+ * Copyright (C) 2023 Cloud Software Group
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
 let _ =
   let inet_addr = Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 9411) in
   let read_body ic file len =
--- ocaml/xs-trace/xs_trace.ml
+++ ocaml/xs-trace/xs_trace.ml
@@ -1,3 +1,17 @@
+(*
+ * Copyright (C) 2023 Cloud Software Group
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
 let _ =
   if Array.length Sys.argv <> 4 then (
     Printf.eprintf "Usage: %s cp/mv <origin> <destination>\n" Sys.argv.(0) ;
