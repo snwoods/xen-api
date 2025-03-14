c14fbd542cd2314f6c285ebb31ffb25b98313e83
Steven Woods <steven.woods@cloud.com>
2025-03-14 18:17:54 +0000

    CP-53555: Split unplug atomic into deactivate/detach
    Signed-off-by: Steven Woods <steven.woods@cloud.com>

--- ocaml/xenopsd/lib/xenopsd.ml
+++ ocaml/xenopsd/lib/xenopsd.ml
@@ -288,6 +288,11 @@ let options =
     , (fun () -> string_of_bool !Xenops_server.xenopsd_vbd_plug_legacy)
     , "False if we want to split the plug atomic into attach/activate"
     )
+  ; ( "xenopsd-vbd-unplug-legacy"
+    , Arg.Bool (fun x -> Xenops_server.xenopsd_vbd_unplug_legacy := x)
+    , (fun () -> string_of_bool !Xenops_server.xenopsd_vbd_unplug_legacy)
+    , "False if we want to split the unplug atomic into deactivate/detach"
+    )
   ]
 
 let path () = Filename.concat !sockets_path "xenopsd"
